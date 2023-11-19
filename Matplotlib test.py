27089ae7c29897a8d005b01e565ee6a7698d4cd7
Alireza Razavi <114526330+qMRoRZp@users.noreply.github.com>
2023-11-19 13:04:44 +0330

    Update Matplotlib test.py

--- Matplotlib test.py
+++ Matplotlib test.py
@@ -7,13 +7,24 @@ def update(frame):
     ax.bar(range(len(frame)), frame, color='blue')
     plt.title("Three-Way Partition Sort")
 
+def quicksort(lst):
+    if len(lst) <= 1:
+        return lst
+
+    pivot = lst[0]
+    less = [x for x in lst[1:] if x < pivot]
+    equal = [x for x in lst if x == pivot]
+    greater = [x for x in lst[1:] if x > pivot]
+
+    return quicksort(less) + equal + quicksort(greater)
+
 def three_way_partition_sort_visualized(lst):
     if len(lst) <= 2:
         return sorted(lst)
 
     l1 = lst[:len(lst)//3]
-    ln_minus_m = lst[len(lst)//3:2len(lst)//3]
-    ln = lst[2len(lst)//3:]
+    ln_minus_m = lst[len(lst)//3:2*len(lst)//3]
+    ln = lst[2*len(lst)//3:]
 
     if l1:
         l1_sorted = quicksort(l1)
@@ -35,11 +46,15 @@ def three_way_partition_sort_visualized(lst):
 
 data = [1, 5, 8, 4, 2]
 
-
 sorting_steps = [three_way_partition_sort_visualized(data[:i+1]) for i in range(len(data))]
 
-
 fig, ax = plt.subplots()
-animation = FuncAnimation(fig, update, frames=sorting_steps, repeat=False)
+
+def init():
+    ax.set_xlim(0, len(data))
+    ax.set_ylim(0, max(data) + 1)
+    return []
+
+animation = FuncAnimation(fig, update, frames=sorting_steps, init_func=init, repeat=False)
 
 plt.show()
